50b9a48ee41e293c929ea4125e531efd83b2867b
AddictedToBattlestar <addictedtobsg@gmail.com>
2025-04-23 20:48:44 -0400

    session-3-styling-the-landing-screen done

--- expo-go-real-time-chat/app/index.tsx
+++ expo-go-real-time-chat/app/index.tsx
@@ -1,34 +1,64 @@
 import { useState } from "react";
-import { Pressable, Text, TextInput, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   const [userName, setUserName] = useState<string>('');
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Welcome! Please enter a user name to proceed.</Text>
+      <Text style={styles.welcomeText}>Welcome! Please enter a user name to proceed.</Text>
 
       <TextInput
+          style={styles.userNameInput}
           value={userName}
           onChangeText={(text) => {
-            setUserName(text);
+              setUserName(text);
           }}
           placeholder={"Enter your desired user name here"}
           placeholderTextColor={"grey"}
       />
 
-      <Pressable 
+      <Pressable
+          style={styles.continueButton}
           onPress={() => {
             alert(`Continue pressed. Current userName: ${userName}`)
           }}
       >
-          <Text>Continue</Text>
+          <Text style={styles.continueButtonText}>Continue</Text>
       </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    gap: 25
+  },
+  welcomeText: {
+    marginTop: 25
+  },
+  userNameInput: {
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  continueButton: {
+    // Makes the input fill the entire horizontal width of the parent container.
+    width: "100%",
+    // Gives rounded edges to the border around this element.
+    borderRadius: 10,
+    // Places a padding inside the element so that whatever is entered does not touch the borders.
+    padding: 10,
+    // Aligns the content in the button to be shown in the horizontal center of the screen.
+    alignItems: "center",
+    // Assigns a prominent color to the background to indicate this is an interactive button.
+    backgroundColor: "blue",
+  },
+  continueButtonText: {
+      color: "white"
+  }
+});
